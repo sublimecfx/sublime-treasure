ba72213f56b31b44a817d75efd373d37501737a9
Atoshi <atoshit@protonmail.com>
2025-05-19 22:54:58 +0200

    Refactor locale loading in event and command handlers to improve code organization and ensure consistent access to localized strings.

--- sources/client/events.lua
+++ sources/client/events.lua
@@ -1,6 +1,5 @@
-local T <const> = sl.loadLocale()
-
 RegisterNetEvent('sl_treasurehunt:startEvent', function(zone, treasureCoords)
+    local T <const> = sl.loadLocale()
     if sl.treasureEvent then return warn(T['start_failed_already_started']) end    
     sl.startTreasureEvent(zone, treasureCoords)
 end)
--- sources/server/commands.lua
+++ sources/server/commands.lua
@@ -1,11 +1,11 @@
 local CONFIG <const> = sl.loadConfig('main')
-local T <const> = sl.loadLocale()
 
 RegisterCommand(CONFIG.manualCommand, function(s, a, rc)
     local hasPermission = sl.require('modules.perms.server')
 
     if s ~= 0 then
         if not hasPermission(s) then
+            local T <const> = sl.loadLocale()
             local framework = sl.loadBridge()
             return framework.notify(s, T['no_permission'], 'error', 5000)
         end
